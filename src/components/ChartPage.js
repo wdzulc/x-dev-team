157b2374cc0bff330929cea3ac0f4642a682d296
Wilberto Dzul <wilberto.dzul@gmail.com>
2016-06-05 02:58:50 -0500

    Ejemplo con 2 gráficas

--- src/components/ChartPage.js
+++ src/components/ChartPage.js
@@ -1,24 +1,67 @@
 import React from 'react';
 import {Line as LineChart} from 'react-chartjs';
 
+let data = [ {
+    period: "2015/10",
+    state: "nacional",
+    value: "0.427473608955"
+},
+{
+    period: "2015/11",
+    state: "nacional",
+    value: "0.198357253439"
+}
+]
+
+let dataYuc = [
+    {
+    period: "2015/10",
+    state: "yucatan",
+    value: "8.168076744638"
+},
+{
+    period: "2015/11", 
+    state: "yucatan",
+    values: "2.384237617361"
+}
+]
+
+let dataNacionalValues = data.map( (object) => {
+    return object.value;
+});
+
+let dataYucValues = dataYuc.map( (object) => {
+    return object.value;
+});
+
 
 let chartData = {
-	labels: ["January", "February", "March", "April", "May", "June", "July"],
+
+	labels: ["2015/10", "2015/11"],
     datasets: [
         {
-            label: "My First dataset",
+            label: "Nacional",
             backgroundColor: "rgba(255,99,132,0.2)",
             borderColor: "rgba(255,99,132,1)",
             borderWidth: 1,
             hoverBackgroundColor: "rgba(255,99,132,0.4)",
             hoverBorderColor: "rgba(255,99,132,1)",
-            data: [65, 59, 80, 81, 56, 55, 40],
-        }
+            data: dataNacionalValues,
+        },
+        {
+            label: "Yucatan",
+            backgroundColor: "rgba(151,187,205,1)",
+            borderColor: "rgba(255,99,132,1)",
+            borderWidth: 1,
+            hoverBackgroundColor: "rgba(255,99,132,0.4)",
+            hoverBorderColor: "rgba(255,99,132,1)",
+            data: dataYucValues,
+        },
     ]
     };
 
 let chartOptions = {
-        
+        responsive: true,
     };
 
 class ChartPage extends React.Component {
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,12 +5,10 @@ import ChartPage from './ChartPage.js';
 const HomePage = () => {
   return (
     <div>
-      <h1>React Slingshot</h1>
+      <h1>Review</h1>
 
-      <h2>Get Started</h2>
+      <h2></h2>
       <ol>
-        <li>Review the <Link to="fuel-savings">demo app</Link></li>
-        <li>Remove the demo and start coding: npm run remove-demo</li>
         <ChartPage/>
       </ol>
     </div>
